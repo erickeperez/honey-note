382b8b3060d33fd8320d6cf5987c74a04800f15e
Erick Perez <eperez@Ericks-MBP.fios-router.home>
2020-05-27 12:03:16 -0400

    Added dependecies

--- src/index.js
+++ src/index.js
@@ -4,6 +4,25 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+const firebase = require('firebase');
+require('firebase/firestore');
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDMwi5_AKHtzphA-cZZvV0oNsrHKnczJTg",
+  authDomain: "honeynotes-abb4b.firebaseapp.com",
+  databaseURL: "https://honeynotes-abb4b.firebaseio.com",
+  projectId: "honeynotes-abb4b",
+  storageBucket: "honeynotes-abb4b.appspot.com",
+  messagingSenderId: "487790571048",
+  appId: "1:487790571048:web:ba1fa94f935a4430220f1d",
+  measurementId: "G-RQ4BDG085E"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
